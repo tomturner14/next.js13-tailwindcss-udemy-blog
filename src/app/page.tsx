b1eb2b5a44ac471674438511f63bba5d1a5aca51
tomturner14 <tomfrom1994@gmail.com>
2024-06-27 03:03:32 +0900

    44まで

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,17 @@ import ArticleList from "./components/ArticleList";
 import { getAllArticles } from "@/blogAPI";
 import { useEffect } from "react";
 import { Http2ServerRequest } from "http2";
+import { supabase } from "@/utils/supabaseClient";
 
 export default async function Home() {
-  const articles = await getAllArticles();
+  // const articles = await getAllArticles();
+  // console.log(supabase)
+
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+
+  const res = await fetch(`${API_URL}/api`, { cache: "no-store" });
+  const articles = await res.json();
 
   return (
     <div className="md:flex">
--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -1,10 +1,80 @@
+import { notFound } from "next/navigation";
 import { Article } from "./types";
 
 export const getAllArticles = async (): Promise<Article[]> => {
   const res = await fetch(`http://localhost:3001/posts`, { cache: "no-store" }); //SSR
 
-  throw new Error("エラーが発生しました。")
+  if (!res.ok) {
+    throw new Error("エラーが発生しました。");
+  }
+
+  await new Promise((resolve) => setTimeout((resolve, 1500)));
 
   const articles = await res.json();
   return articles;
+};
+
+export const getDetailArticles = async (id: string): Promise<Article> => {
+  const res = await fetch(`http://localhost:3001/posts/${id}`, {
+    next: { revalidate: 60 },
+  }); //SSR
+
+  if(res.status === 404) {
+    notFound();
+  }
+  if (!res.ok) {
+    throw new Error("エラーが発生しました。");
+  }
+
+  await new Promise((resolve) => setTimeout((resolve, 1000)));
+
+  const article = await res.json();
+  return article;
+};
+
+export const createArticle = async (
+  id: string,
+  title: string,
+  content: string
+): Promise<Article> => {
+
+  const currentDatetime = new Date().toISOString();
+
+  const res = await fetch(`http://localhost:3001/posts`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id, title, content, createdAt: currentDatetime })
+  });
+
+  if(res.status === 404) {
+    notFound();
+  }
+  if (!res.ok) {
+    throw new Error("エラーが発生しました。");
+  }
+
+  await new Promise((resolve) => setTimeout((resolve, 1000)));
+
+  const newArticle = await res.json();
+  return newArticle;
+};
+
+export const deleteArticle = async (id: string): Promise<Article> => {
+  const res = await fetch(`http://localhost:3001/posts/${id}`, {
+    method: "DELETE",
+  });
+
+  if(res.status === 404) {
+    notFound();
+  }
+  if (!res.ok) {
+    throw new Error("エラーが発生しました。");
+  }
+
+  await new Promise((resolve) => setTimeout((resolve, 1000)));
+
+  const deleteArticle = await res.json();
+  return deleteArticle;
 };
